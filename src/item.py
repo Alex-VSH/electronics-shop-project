57b18b19c8b139504525fb7954b8e561ca512514
Alex_Volk-Shevalye <aisb@tut.by>
2023-08-05 18:52:22 +0300

    changes for homework #2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -15,7 +19,34 @@ class Item:
         """
         self.quantity = quantity
         self.price = price
-        self.name = name
+        self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        operation_path = os.path.join(os.path.dirname(__file__), "items.csv")
+        with open(operation_path, 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            reader = list(reader)
+            for i in range(1, len(reader)):
+                new_item = Item(reader[i][0], float(reader[i][1]), int(reader[i][2]))
+                Item.all.append(new_item)
+
+
+    @staticmethod
+    def string_to_number(number):
+        return int(float(number))
+
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:       # Тут можно сделать короче, но задание просит именно проверку на длинну
+            self.__name = name[:10]
+        else:
+            self.__name = name
 
     def calculate_total_price(self) -> float:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -14,3 +14,22 @@ def test_apply_discount():
     Item.pay_rate = 0.7
     item1.apply_discount()
     assert item1.price == 21000.0
+
+
+def test_string_to_number():
+    assert Item.string_to_number('2') == 2
+
+
+def test_name():
+    item1.name = "Печь"
+    assert item1.name == "Печь"
+    item1.name = 'Микроволновка'
+    assert item1.name == 'Микроволно'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    item_test = Item.all[3]
+    assert item_test.name == 'Мышка'
+    assert item_test.price == 50.0
+    assert item_test.quantity == 5
