6f04c401b4ef1020be3e885313982516add20661
Alex_Volk-Shevalye <aisb@tut.by>
2023-08-13 16:28:17 +0300

    changes for homework #5

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,13 +4,19 @@ from src.item import Item
 class MixinLog:
 
     def __init__(self):
-        self.language = 'EN'
+        self.__language = 'EN'
+
+    @property
+    def language(self):
+        return self.__language
 
     def change_lang(self):
-        if self.language == "EN":
-            self.language = "RU"
-        elif self.language == "RU":
-            self.language = "EN"
+        if self.__language == "EN":
+            self.__language = "RU"
+            return self
+        elif self.__language == "RU":
+            self.__language = "EN"
+            return self
 
 
 class Keyboard(Item, MixinLog):
